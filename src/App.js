e7668a391f845ddf6d8ed0027abcfd5f82d90533
samagragupta <samagraguptajpr@gmail.com>
2018-12-10 11:05:50 +0530

    Added box and list div

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import Box from './Components/Box';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Box />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Components/Box.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Box.css'
+
+class Box extends Component {
+    render () {
+        return (
+            <div className="boxbody">
+                <div className="list">
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Box;
