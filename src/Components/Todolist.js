5ef22e4f278ff7926df8ee5b4af6c93fe0112a5a
samagragupta <samagraguptajpr@gmail.com>
2018-12-14 09:57:58 +0530

    delete working

--- src/Components/Todolist.js
+++ src/Components/Todolist.js
@@ -3,10 +3,34 @@ import './Todolist.css';
 import Bin from './rubbish-bin.png'
 
 class Todolist extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            display : false,
+        }
+    }
+
+    displayModalHandler = () => {
+        var displayValue = !this.state.display;
+        this.setState ({
+            display : displayValue
+        })
+      }
+
     render () {
+
+        var style = {
+            display: 'block',
+          }
+      
+          if (this.state.display){
+              style.display = 'none';
+          }
+
         return (
-            <div className="todolist">
-                <img src={Bin} className="bin" alt="Bin" />
+            <div className="todolist" style={style}>
+                <img src={Bin} className="bin" alt="Bin" onClick={this.displayModalHandler} />
                 <div className="task">
                     {this.props.tasks}
                 </div>
