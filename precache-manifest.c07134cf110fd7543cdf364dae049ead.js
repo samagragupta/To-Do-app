c18d17d73e0c673c4907ba69435e0b775f7a8fa8
samagragupta <samagraguptajpr@gmail.com>
2018-12-14 12:51:17 +0530

    Updates

---
+++ precache-manifest.c07134cf110fd7543cdf364dae049ead.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "01d3f2e62a25d730b8a3",
+    "url": "/To-Do-app/static/js/runtime~main.01d3f2e6.js"
+  },
+  {
+    "revision": "f0763e44e59abb0ea18e",
+    "url": "/To-Do-app/static/js/main.f0763e44.chunk.js"
+  },
+  {
+    "revision": "66e0bc75574e122856a4",
+    "url": "/To-Do-app/static/js/1.66e0bc75.chunk.js"
+  },
+  {
+    "revision": "f0763e44e59abb0ea18e",
+    "url": "/To-Do-app/static/css/main.be33ae99.chunk.css"
+  },
+  {
+    "revision": "f31e24cb1eb01e2800edcd5d54d3420c",
+    "url": "/To-Do-app/index.html"
+  }
+];
