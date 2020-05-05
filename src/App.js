a24706b47e7bf45bc30222730c1da90d0d8ee658
= <=>
2020-05-05 19:32:38 +0530

    Added more features

--- src/App.js
+++ src/App.js
@@ -10,17 +10,26 @@ class App extends Component {
     super(props);
     this.state = {
         inputValue: '',
+        datevalue: '',
         itemArray: [],
         display : false,
+        checkValues: '',
+        tickValue1: false,
+        tickValue2: false,
+        tickValue3: false,
     }
 }
 
   onAddTask = () => {
     var displayValue = !this.state.display;
     this.setState({
-      itemArray: [...this.state.itemArray, this.state.inputValue],
+      itemArray: [...this.state.itemArray, {value: this.state.inputValue, datetime: this.state.datevalue, checkvalue: this.state.checkValues}],
       display : displayValue,
       inputValue : '',
+      datevalue: '',
+      tickValue1: false,
+      tickValue3: false,
+      tickValue2: false,
     })
   }
 
@@ -29,12 +38,50 @@ class App extends Component {
       inputValue: event.target.value
     });
   }
+  
+  updateDateValue = (event) => {
+    this.setState({
+      datevalue: event.target.value
+    });
+  }
 
   displayModalHandler = () => {
     var displayValue = !this.state.display;
     this.setState ({
         display : displayValue
     })
+  }
+
+  // tickvalue = 'low';
+
+  updateCheck = (event) => {
+    this.setState({
+      checkValues: event.target.value,
+    });
+    console.log(event.target.value)
+    // tickvalue = event.target.value;
+    if(event.target.value === 'low'){
+      this.setState({
+        tickValue1: true,
+        tickValue2: false,
+        tickValue3: false,
+      });
+    }
+    if(event.target.value === 'medium'){
+      this.setState({
+        tickValue2: true,
+        tickValue1: false,
+        tickValue3: false,
+      });
+    }
+    if(event.target.value === 'high'){
+      this.setState({
+        tickValue1: false,
+        tickValue2: false,
+        tickValue3: true,
+      });
+    }
+
   }
 
   render() {
@@ -47,6 +94,7 @@ class App extends Component {
         style.display = 'block';
     }
 
+
     return (
       <div className="App">
         <Box createTodo = {
@@ -54,14 +102,16 @@ class App extends Component {
             {this.state.itemArray.map((itemArr) => {
               return (
                 <div className="box">
-                  <Todolist tasks = {itemArr} />
+                  <Todolist tasks = {itemArr.value} date = {itemArr.datetime} priority = {itemArr.checkvalue}/>
                 </div>
               )
             })}
           </div>
           }>
         </Box>
-        <Add addTask = {this.onAddTask} inputValues = {this.updateInputValue}
+        <Add addTask = {this.onAddTask} inputValues = {this.updateInputValue} inputDeadlines = {this.updateDateValue}
+          inputDeadline = {this.state.datevalue} checkOnChange = {this.updateCheck} tick1 = {this.state.tickValue1}
+          tick2 = {this.state.tickValue2} tick3 = {this.state.tickValue3} 
          inputV = {this.state.inputValue} displayStyle = {style} displayn = {this.displayModalHandler} />
       </div>
     );
--- src/Components/AddModal.js
+++ src/Components/AddModal.js
@@ -3,11 +3,59 @@ import './AddModal.css';
 
 class AddModal extends Component {
 
-    render () {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            priority: ''
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        // this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({
+            priority: event.target.value
+        });
+        // this.props.checkbox();
+    }
+
+    render() {
         return (
             <div className="addmodal">
                 <label className="head">Add my ToDo</label>
-                <input type ="text" className="text" value={this.props.inputV} onChange={this.props.inputValues} />
+                <input type="text" className="text" value={this.props.inputV} onChange={this.props.inputValues} />
+                <input type="date" placeholder="Deadline YYYY-MM-DD" value={this.props.inputDeadline} onChange={this.props.inputDeadlines} />
+                <div>
+                    <label>
+                        <input
+                            type="radio"
+                            value= "low"
+                            checked = {this.props.tick1}
+                            onChange={this.props.checkOnChange}
+                        />
+                                Low
+                            </label>
+                    <label>
+                        <input
+                            type="radio"
+                            value= "medium"
+                            checked = {this.props.tick2}
+                            onChange={this.props.checkOnChange}
+                        />
+                                Medium
+                            </label>
+                    <label>
+                        <input
+                            type="radio"
+                            value= "high"
+                            checked = {this.props.tick3}
+                            onChange={this.props.checkOnChange}
+                        />
+                                High
+                            </label>
+                </div>
                 <button type="submit" className="addbutton" onClick={this.props.addTask} >Add the task</button>
             </div>
         );
