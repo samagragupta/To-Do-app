c5f8291aeeb68af551e9fef30024c6b8c4a7e208
Neeraj Rajpurohit <neerajrajpurohitnn@gmail.com>
2020-10-01 12:05:49 +0530

    using-functional-comp

--- src/Components/Box.js
+++ src/Components/Box.js
@@ -1,28 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Box.css';
-// import Todolist from './Todolist';
 
-class Box extends Component {
-
-    render () {
-        return (
-            <div className="boxbody">
-                <div className="list">
-                    {/* <Todolist />
-                    <Todolist {...this.props} />
-                    <Todolist {...this.props} />
-                    {/* <Todolist />
-                    <Todolist />
-                    <Todolist />
-                    <Todolist />
-                    <Todolist />
-                    <Todolist />
-                    <Todolist /> */}
-                    {this.props.createTodo}
-                </div>
+const Box  = props => 
+        <div className="boxbody">
+            <div className="list">
+                {props.createTodo}
             </div>
-        );
-    }
-}
+        </div>
 
 export default Box;
